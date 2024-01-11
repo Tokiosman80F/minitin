584bc266520699aa85793f0a1c52d6780d9eb06c
Tokiosman80F <tokiosman96@gmail.com>
2024-01-12 03:04:00 +0600

    navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,31 @@
-
-import Logo from "../../assets/images/logo/logo.png"
-import { Link } from 'react-router-dom';
+import Logo from "../../assets/images/logo/logo - Copy.png";
+import { Link } from "react-router-dom";
 import { BsFillPersonFill } from "react-icons/bs";
 const Navbar = () => {
-    return (
-        <div className='flex'>
-            <div> <img src={Logo} alt="" /> Mini Tin</div>
-            {/* nav link */}
-            <div className='flex '>
-                <Link to="">Home</Link>
-                <Link to="">All Toy</Link>
-                <Link to="">My Toys</Link>
-                <Link to="">Add Toy</Link>
-                <Link to="">Blogs</Link>
-                <Link to=""></Link>
-            </div>
-            {/* user & login */}
-            <div>
-                <div></div>
-                <div><BsFillPersonFill /></div>
-            </div>
+  return (
+    <div className="flex justify-between items-center my-10  border-b-2 pb-2">
+      <div className="flex items-center ">
+        <img src={Logo} alt="MiniTin Logo" />
+        <div className="text-center text-3xl font-bold">MiniTin</div>
+      </div>
+      {/* nav link */}
+      <div className="flex gap-10 ">
+        <Link to="">Home</Link>
+        <Link to="">All Toy</Link>
+        <Link to="">My Toys</Link>
+        <Link to="">Add Toy</Link>
+        <Link to="">Blogs</Link>
+        <Link to=""></Link>
+      </div>
+      {/* user & login */}
+      <div>
+        <div></div>
+        <div className="flex items-center gap-1 font-bold text-blue-500">
+          <BsFillPersonFill /> Login
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
